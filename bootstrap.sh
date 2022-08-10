90988be211663ffb2b87c057e81ee84bfd3152f1
Tue Tran <tue1996@gmail.com>
2022-08-10 10:54:20 +0700

    update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 /usr/bin/python3 -m pip install virtualenv
 /usr/bin/python3 -m virtualenv venv
+source venv/bin/activate
 pip install requirements.txt
 touch .env
 export FLASK_APP=api.py
-source venv/bin/activate
 flask run -h 0.0.0.0
