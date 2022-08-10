41e27ec4ee9c1b9034e1c0768aecf30f11d598d7
Tue Tran <tue1996@gmail.com>
2022-08-10 11:00:02 +0700

    add -r to requirements.txt

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,7 +2,7 @@
 /usr/bin/python3 -m pip install virtualenv
 /usr/bin/python3 -m virtualenv venv
 . venv/bin/activate
-pip install requirements.txt
+pip install -r requirements.txt
 touch .env
 export FLASK_APP=api.py
 flask run -h 0.0.0.0
