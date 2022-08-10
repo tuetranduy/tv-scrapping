2bd7a5ff1b0a58801a140b293a44156f3a560d21
Tue Tran <tue1996@gmail.com>
2022-08-10 12:49:57 +0700

    add tvdatafeed via github

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,6 +3,7 @@
 /usr/bin/python3 -m virtualenv venv
 . venv/bin/activate
 pip install -r requirements.txt
+pip install --upgrade --no-cache-dir git+https://github.com/StreamAlpha/tvdatafeed.git
 touch .env
 export FLASK_APP=api.py
 flask run -h 0.0.0.0
