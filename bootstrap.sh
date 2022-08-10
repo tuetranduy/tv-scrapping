89a63b3056f87d4e3ebf310c383e3abe61d4376a
Tue Tran <tue1996@gmail.com>
2022-08-10 10:53:04 +0700

    update venv name

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 /usr/bin/python3 -m pip install virtualenv
-/usr/bin/python3 -m virtualenv virtual
+/usr/bin/python3 -m virtualenv venv
 pip install requirements.txt
 touch .env
 export FLASK_APP=api.py
