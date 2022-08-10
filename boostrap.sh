816e68622b4109f82d52f9d29050b01e6c937b00
Tue Tran <tue1996@gmail.com>
2022-08-10 14:02:56 +0700

    update

--- boostrap.sh
+++ boostrap.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 echo '===== cloning source code ... ====='
 git clone https://github.com/tuetranduy/tv-scrapping.git
+sudo chmod 775 -R tv-scrapping
 cd tv-scrapping
 ./install.sh
 
@@ -11,4 +12,4 @@ echo 'TV_PASSWORD=' >> .env
 
 echo '===== starting application ... ====='
 export FLASK_APP=api.py
-flask run -h 0.0.0.0
+pythonw -m flask run -h 0.0.0.0
