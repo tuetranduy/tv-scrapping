dc4b40a95310d5cc0ffec038179ae1141f581b30
Tue Tran <tue1996@gmail.com>
2022-08-10 13:40:29 +0700

    improving deploy script

--- boostrap.sh
+++ boostrap.sh
@@ -1,4 +1,14 @@
 #!/bin/sh
+echo '===== cloning source code ... ====='
 git clone https://github.com/tuetranduy/tv-scrapping.git
 cd tv-scrapping
 ./install.sh
+
+echo '===== putting username and password to .env file ... ====='
+touch .env
+echo 'TV_USERNAME=tuetranduy' >> .env
+echo 'TV_PASSWORD=tuetranduy123' >> .env
+
+echo '===== starting application ... ====='
+export FLASK_APP=api.py
+flask run -h 0.0.0.0
--- install.sh
+++ install.sh
@@ -1,13 +1,18 @@
 #!/bin/sh
+
+echo '===== allowing firewall ... ====='
 sudo ufw allow 5000
 sudo ufw allow 80
+
+echo '===== install apache2 ... ====='
 sudo apt-get -y install apache2
+
+echo '===== starting apache2 service ... ====='
 sudo service apache2 restart
+
+echo '===== installing dependencies ... ====='
 /usr/bin/python3 -m pip install virtualenv
 /usr/bin/python3 -m virtualenv venv
 . venv/bin/activate
 pip install -r requirements.txt
 pip install --upgrade --no-cache-dir git+https://github.com/StreamAlpha/tvdatafeed.git
-touch .env
-export FLASK_APP=api.py
-flask run -h 0.0.0.0
